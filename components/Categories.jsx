b02ebf4f46a1b02463f223448ac6d850ba4198c0
Enzo Alves Turcovic <enzoturcovic@gmail.com>
2025-03-27 11:45:57 -0300

    Create Categories

--- components/Categories.jsx
+++ components/Categories.jsx
@@ -1,1 +1,13 @@
 import styles from '../styles/Categories.module.css';
+
+const Categories = () => {
+    return (
+        <div className={styles.categories}>
+            <h1 className={styles.title}>Categorias</h1>
+            <ul className={styles.list}>
+                <li className={styles.item}>Aventura</li>
+                <li className={styles.item}>Romance</li>
+            </ul>
+        </div>
+    );
+}
