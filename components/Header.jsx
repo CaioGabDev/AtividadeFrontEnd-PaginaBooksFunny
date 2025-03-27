9ae6b344ab5ac0258bc507597d16f2b12051dbcf
Bernardo <sardinhaber@gmail.com>
2025-03-27 10:25:36 -0300

    feat: create header

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,9 +1,21 @@
-import styles from './Header.module.css';
 
-const Header = () => {
+
+
+export default function Header(){
     return (
-        <header className={styles.header}>
-            <h1>Caio Lacerda</h1>
+        <header >
+            <div>
+                <h1>📚Books and funny</h1>
+                <p>Onde ler é a maior diversão</p>
+            </div>
+            <ul>
+                <li>Home</li>
+                <li>Books</li>
+                <li>Authors</li>
+                <li>Genres</li>
+            </ul>
+            <input type="text" />
+            <img src="" alt="" />
         </header>
     );
 };
