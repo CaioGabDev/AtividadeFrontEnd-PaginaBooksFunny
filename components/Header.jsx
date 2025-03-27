c5132a9985c393ee75caf65f9ec92d69db8d7b2c
Caio Lacerda <lacerdacaiogabriel@gmail.com>
2025-03-27 09:33:40 -0300

    first commit

---
+++ components/Header.jsx
@@ -0,0 +1,9 @@
+import styles from './Header.module.css';
+
+const Header = () => {
+    return (
+        <header className={styles.header}>
+            <h1>Caio Lacerda</h1>
+        </header>
+    );
+};
