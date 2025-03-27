946c7a10353cc0fb23e21389e8ea6d1451adc990
Enzo Alves Turcovic <enzoturcovic@gmail.com>
2025-03-27 11:26:58 -0300

    Update components/Footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,12 +1,14 @@
-import styles from '.Footer.module.css';
+import styles from '../styles/Footer.module.css';
 
 const Footer = () => {
     return (
         <footer className={styles.footer}>
             <h1 className={styles.title}>BooksFunny📚</h1>
             <p className={styles.subtitle}>O melhor lugar para encontrar sua próxima história!</p>
-            <p className={styles.copyright}>2025 BooksFunny. Todos os direitos reservados</p>
-            <p className={styles.license}>Termos de uso | Política de privacidade</p>
+            <div className={styles.license}>
+                <p className={styles.copyright}>2025 BooksFunny. Todos os direitos reservados</p>
+                <p className={styles.terms}>Termos de uso | Política de privacidade</p>
+            </div>
         </footer>
     );
 }
