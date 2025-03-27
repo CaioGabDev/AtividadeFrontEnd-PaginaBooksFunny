d8c3d09694311b549c7a05fdf4744553141b0571
Bernardo <sardinhaber@gmail.com>
2025-03-27 10:50:35 -0300

    feat: inicialization style header

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,17 @@
 
-import Card from "../components/Card"
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+    const router = useRouter();
+
+    useEffect(() => {
+        router.push("/login");
+    }, [router]);
+	
 return (
-    <div className={styles.container}>
-        <Card />
-    </div>
-    )
+	<p>Redirecting …</p>
+);
 }
--- components/Header.jsx
+++ components/Header.jsx
@@ -1,20 +1,22 @@
-
+import styles from '../styles/Header.module.css';
 
 
 export default function Header(){
     return (
-        <header >
-            <div>
+        <header className={styles.header}> 
+            <div className={styles.logo}>
                 <h1>📚Books and funny</h1>
                 <p>Onde ler é a maior diversão</p>
             </div>
+            <div className={styles.navegation}>
             <ul>
                 <li>Home</li>
                 <li>Books</li>
                 <li>Authors</li>
                 <li>Genres</li>
             </ul>
-            <input type="text" />
+            </div>
+            <input className={styles.input} type="text" />
             <img src="" alt="" />
         </header>
     );
