e6797b9fd24ecdcc144dea08a752de577d51f843
Caio Lacerda <lacerdacaiogabriel@gmail.com>
2025-04-03 11:37:29 -0300

    feat: add lancamentos

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -25,8 +25,12 @@ export default function Home() {
                     <Card image="https://m.media-amazon.com/images/I/710HGTP2esL._AC_UF1000,1000_QL80_.jpg" title="Livro do MAIOR" author="Enzo Turcovic" price="R$ 1,00" rating={0.1} />
                 </div>
             </div>
-
+            <div className="titleSection">
+                    <h1>Categorias Populares</h1>
+                    <p className={styles.viewall}>Ver todos</p>
+                </div>
             <section className="categorias">
+                
                 <CardCategorie
                     emoji="🚀"
                     categoria="Ficção científica"
@@ -56,6 +60,17 @@ export default function Home() {
                     categoria="Autoajuda"
                 />
             </section>
+            <div className="section">
+                <div className="titleSection">
+                    <h1>Lançamentos</h1>
+                    <p className={styles.viewall}>Ver todos</p>
+                </div>
+                <div className="cards">
+                    <Card image="https://m.media-amazon.com/images/I/81s4Vp0dx6L._AC_UF1000,1000_QL80_.jpg" title="Livro do cr7" author="CR7" price="R$ 120,00" rating={5} />
+                    <Card image="https://m.media-amazon.com/images/I/81NWjF14ABL._AC_UF1000,1000_QL80_.jpg" title="Livro Messi" author="Messi" price="R$ 160,00" rating={5} />
+                    <Card image="https://cdn.awsli.com.br/600x450/1152/1152486/produto/240606930/catapulta-pb4hv8vfny.jpg" title="Livro do pelé" author="Pele" price="R$ 1,00" rating={0.1} />
+                </div>
+            </div>
             <Footer />
         </div>
     );
