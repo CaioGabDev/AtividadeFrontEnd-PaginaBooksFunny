0a95e3ffa086d1617c31f14e4f1ce865053e80c6
Bernardo <sardinhaber@gmail.com>
2025-04-03 08:50:58 -0300

    refactor: logo

--- components/Header.jsx
+++ components/Header.jsx
@@ -6,8 +6,7 @@ export default function Header(){
     return (
         <header className={styles.header}> 
             <div className={styles.logo}>
-                <h1>📚Books and funny</h1>
-                <p>Onde ler é a maior diversão</p>
+                <h1>📚BooksFunny</h1>
             </div>
             <Navegation />
             <input className={styles.input} type="text" placeholder='Buscar livros, autores...        🔎' />
